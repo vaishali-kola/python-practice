c45715bbc948d9a10eaa6bceed2dfd67c6b99aec
vaishali-kola <24811a05b1@gmail.com>
2026-02-11 10:44:50 +0530

    Delete cs2.py

--- cs2.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a value:"))
-b=int(input("enter b value:"))
-c=int(input("enter c value:"))
-if a>b and a>c:
-    print("a is greater")
-elif b>a and b>c:
-    print("b is greater")
-else:
-    print("c is greater")
