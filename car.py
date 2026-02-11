36302891c58470f9b9e37e3916c64d371907d2c4
vaishali-kola <24811a05b1@gmail.com>
2026-02-11 10:42:49 +0530

    Delete car.py

--- car.py
+++
@@ -1,9 +0,0 @@
-class car:
-    def __init__(self,name,color):
-       self.name=name
-       self.color=color
-    def show(self):
-       print("name:",self.name)
-       print("color:",self.color)
-m1=car("maruti suzuki","black")
-m1.show()
