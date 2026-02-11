a66b7458430ef374cf6c07a1aef7a4b1daa43670
vaishali-kola <24811a05b1@gmail.com>
2026-02-11 11:18:42 +0530

    Delete mobile.py

--- mobile.py
+++
@@ -1,8 +0,0 @@
-class Mobile:
-    def __init__(self,brand,price):
-        self.brand=brand
-        self.price=price
-m1=Mobile("vivo",15000)
-m2=Mobile("redmi",13000)
-print(m1.brand,m1.price)
-print(m2.brand,m1.price)
