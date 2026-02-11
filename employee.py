4af5e428c21bcbbb24b3d75d73d34128e18e92c5
vaishali-kola <24811a05b1@gmail.com>
2026-02-11 10:51:57 +0530

    Delete employee.py

--- employee.py
+++
@@ -1,9 +0,0 @@
-class Employee:
-    def __init__(self,id,salary):
-        self.id=id
-        self.salary=salary
-    def show(self):
-        print("id:",self.id)
-        print("salary:",self.salary)
-e1=Employee(111,50000)
-e1.show()
